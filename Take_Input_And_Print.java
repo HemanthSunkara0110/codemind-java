8e9de317e2436a8f307e406482e2c89596c79d94
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-08-09 09:14:45 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String s1=s.nextLine();         System.out.println(s1);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        System.out.println(s1);
+        
+        
+    }
+}
