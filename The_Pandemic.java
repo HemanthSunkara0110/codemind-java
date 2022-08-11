ae55eebbad7b1e9181289b3626bd470fa05b42a5
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-08-11 22:48:21 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int h;         h=s.nextInt();         if (h<3)         System.out.println(h-1);         else         System.out.println(2);     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int h;
+        h=s.nextInt();
+        if (h<3)
+        System.out.println(h-1);
+        else
+        System.out.println(2);
+    }
+}
