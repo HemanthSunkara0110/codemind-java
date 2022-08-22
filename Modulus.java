7ecc5376a18f8d4e28bf13804e0b64e5b77d09ae
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-08-22 14:53:54 +0530

    import java.util.Scanner; class hemu {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m;         n=sc.nextInt();         m=sc.nextInt();         System.out.print(n%m);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class hemu
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,m;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        System.out.print(n%m);
+    }
+}
