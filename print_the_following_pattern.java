b8c5c0fa63455bee4f0804d9785407e9ce2cdb37
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-09-12 23:02:40 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();
            for(int i=0;i<n;i++)
            {
                  for(int j=0;j<n;j++)
                  {
                  if(i==j||j==n-i-1)
                  
                  System.out.print("x");
                  else
                      System.out.print("0");
                  }
                  
                       System.out.println();
            }
         
            
             
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
-class Pattern18 {
-
-	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		int n;
-		n=sc.nextInt();
-		
-		for(int i=1;i<=n;i++)		
-		{    int k=i>n?n-i%n:i;
-			for(int j=1;j<=2*n;j=j+1)	
-			{
-				if(j<=n-k)
-					System.out.print(" ");
-			}
-			for(int p=1;p<=2*k-1;p++)
-			{
-					System.out.print(i);
-			}
-			System.out.println();
-			}
-			
-			
-		}
-	
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        
+        for(int i=0;i<n;i++)
+        {
+              for(int j=0;j<n;j++)
+              {
+              if(i==j||j==n-i-1)
+              
+              System.out.print("x");
+              else
+                  System.out.print("0");
+              }
+              
+                   System.out.println();
+        }
+     
+        
+         
+    }
 }
