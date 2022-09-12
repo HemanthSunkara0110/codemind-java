1d090ec48b686189772d1dd4b725ad375a1a6645
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-09-12 20:38:33 +0530

    import java.util.Scanner; class pattern { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int r; 		r=sc.nextInt();
    		for(int i=1;i<=r;i++)		
    		{   
    			for(int j=1;j<=i;j++)
    			
    			{
    			    if(i>=j)
    				System.out.print(j);
    				else
    				System.out.print(" ");
    			}
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
-class main
+class pattern 
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                System.out.print((char)(64+i)+" ");
-            }
-            System.out.println();
-        }
-        
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int r;
+		r=sc.nextInt();
+		
+		for(int i=1;i<=r;i++)		
+		{   
+			for(int j=1;j<=i;j++)
+			
+			{
+			    if(i>=j)
+				System.out.print(j);
+				else
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
 }
