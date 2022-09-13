60c3cc253b0f8a582d7339090eb75e92a90be1aa
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-09-13 22:14:13 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)             a[i]=s.nextInt();         for(int i=0;i<n;i++)         {             int p=1;             for(int j=0;j<n;j++)             {                 if(i!=j)                 {                     p*=a[j];                 }             }             System.out.print(p+" ");         }     } }

---
+++ _Product_of_Array_Except_Self.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int p=1;
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                    p*=a[j]; 
+                }
+            }
+            System.out.print(p+" ");
+        }
+    }
+}
