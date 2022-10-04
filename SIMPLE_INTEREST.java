01003455bbc848535e2292303abebf861731c9ab
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-10-04 12:16:27 +0530

    import java.util.*; class main {     public static void main(String []args)   {       Scanner s=new Scanner(System.in);       int p,t,r,a;       p=s.nextInt();       t=s.nextInt();       r=s.nextInt();       a=(p*t*r)/100;       System.out.println(a);   }
    }

--- SIMPLE_INTEREST.java
+++ SIMPLE_INTEREST.java
@@ -1,19 +1,15 @@
 import java.util.*;
-class Sam
+class main
 {
-    public static void main(String args[])
-    {
-        Scanner s=new Scanner(System.in);
-        
-           int p=s.nextInt();
-          int r=s.nextInt();
-         int t=s.nextInt();
-         int amount=(p*t*r)/100;
-             
-             
-            System.out.println(amount);
-            
-        
-    }
-  
+    public static void main(String []args)
+  {
+      Scanner s=new Scanner(System.in);
+      int p,t,r,a;
+      p=s.nextInt();
+      t=s.nextInt();
+      r=s.nextInt();
+      a=(p*t*r)/100;
+      System.out.println(a);
+  }
+    
 }
