d6c4bbaf7c4cdbcd3c84c310cb392d419e5709a5
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2022-10-15 13:51:27 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int rev=0;         int rem;         while(n>0 || n<0)         {             rem=n%10;             rev=rev*10+rem;             n=n/10;         }         System.out.println(rev);     } }

---
+++ Reverse_Integer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        int rem;
+        while(n>0 || n<0)
+        {
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
