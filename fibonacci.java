69c413e5956b0db063602b2c564ae1f3ea51f1e2
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2023-10-09 12:17:16 +0530

    import java.util.Scanner; public class fibonacci{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=0,b=1,c,n,count=3;         n=sc.nextInt();         c=a+b;         System.out.print(a+" "+b+" "+c+" ");         while(count<n){             a=b;             b=c;             c=a+b;             System.out.print(c+" ");             count++;         }     } }

--- fibonacci.java
+++ fibonacci.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-public class main{
+public class fibonacci{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int a=0;
-        int b=1;
-        int c=a+b;
-        int count=3;
+        int a=0,b=1,c,n,count=3;
+        n=sc.nextInt();
+        c=a+b;
         System.out.print(a+" "+b+" "+c+" ");
         while(count<n){
             a=b;
             b=c;
             c=a+b;
-        System.out.print(c+" ");
-        count++;
+            System.out.print(c+" ");
+            count++;
         }
     }
 }
