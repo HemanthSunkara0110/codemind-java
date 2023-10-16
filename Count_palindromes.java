f7844e98e921885a00f58e8d2592ed7316597b9e
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2023-10-16 22:21:56 +0530

    import java.util.*; public class countPalin{     public static boolean countPalindrome(int n){         int r,temp=0,t=n;         while(n>0){             r=n%10;             temp=temp*10+r;             n=n/10;         }         if(temp==t)         return true;         else         return false;     }     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         int count=0;         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++){             if(countPalindrome(arr[i]))             count++;         }         System.out.println(count);     } }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -1,33 +1,29 @@
-import java.util.Scanner;
-class Main
-{
-        public static boolean isPalindrome(int n)
-        {
-            int sum=0,r,temp=n;
-            while(n>0)
-            {
-                r=n%10;
-                sum=sum*10+r;
-                n=n/10;
-            }
-            if(sum==temp)
-            return true;
-            else
-            return false;
+import java.util.*;
+public class countPalin{
+    public static boolean countPalindrome(int n){
+        int r,temp=0,t=n;
+        while(n>0){
+            r=n%10;
+            temp=temp*10+r;
+            n=n/10;
         }
-    public static void main(String args[])
-  {
-    Scanner sc=new Scanner(System.in);
-    int x[],n,i,count=0;
-    n=sc.nextInt();
-    x=new int[n];
-     for(i=0;i<n;i++)
-    x[i]=sc.nextInt();
-    for(i=0;i<n;i++)
-   {
-    if(isPalindrome(x[i]))
-    count++;
-   }
-   System.out.println(count);
-  }
+        if(temp==t)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(countPalindrome(arr[i]))
+            count++;
+        }
+        System.out.println(count);
+    }
 }
