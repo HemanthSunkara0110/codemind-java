c3291451293520b4572db945c1b1acedddedbc49
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2023-10-16 22:53:07 +0530

    import java.util.*; public class AscendArray{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         int[] arr=new int[n];         for(int i=0;i<n;i++)             arr[i]=sc.nextInt();         for(int i=0;i<n-1;i++){             if(arr[i]>=arr[i+1]){             System.out.println("no");
                System.exit(0);
            }
            }
            System.out.println("yes");
        }
    }

--- Ascending_array.java
+++ Ascending_array.java
@@ -1,24 +1,19 @@
 import java.util.*;
-class elements
-{
-    public static void main(String args[])
-    {
+public class AscendArray{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int x[],i,n,k=0;
-       
-        n=sc.nextInt();
-        x=new int[n];
-        for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
-        for(i=0;i<n-1;i++)
-        {
-        if(x[i]>=x[i+1])
-        {
+        int n=sc.nextInt();
+        int c=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>=arr[i+1]){
             System.out.println("no");
+            
             System.exit(0);
         }
-        
-        }   
+        }
         System.out.println("yes");
     }
 }
