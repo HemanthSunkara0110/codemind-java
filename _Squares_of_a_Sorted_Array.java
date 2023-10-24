9e1edbe3d9cc5af36fb5fdca44edfe3932002f13
HemanthSunkara0110 <110598845+HemanthSunkara0110@users.noreply.github.com>
2023-10-24 20:14:08 +0530

    import java.util.*; public class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         arr[i]=sc.nextInt();         for(int i=0;i<n;i++){             arr[i]=arr[i]*arr[i];         }         Arrays.sort(arr);         for(int i=0;i<n;i++)         System.out.printf("%d ",arr[i]);     }
    }

--- _Squares_of_a_Sorted_Array.java
+++ _Squares_of_a_Sorted_Array.java
@@ -1,18 +1,17 @@
 import java.util.*;
-public class c{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+public class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
         for(int i=0;i<n;i++){
-            arr[i]= sc.nextInt();
-            arr[i] = arr[i]*arr[i];
+            arr[i]=arr[i]*arr[i];
         }
         Arrays.sort(arr);
-        for(int i:arr){
-            System.out.printf("%d ",i);
-        }
-        
-        
+        for(int i=0;i<n;i++)
+        System.out.printf("%d ",arr[i]);
     }
+    
 }
